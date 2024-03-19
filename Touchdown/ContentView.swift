fa625a9979b92d9f9c9696509bf78dade804eee2
DhruvinMulani <mulanidhruvin@gmail.com>
2024-03-19 11:35:10 -0400

    Initial Commit

---
+++ Touchdown/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Touchdown
+//
+//  Created by Dhruvin Mulani on 2024-03-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Dhruvin Mulani on 2024-03-19.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
